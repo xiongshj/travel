df1a61585b475add16382c0b07fc651b80d2998f
xiongshj <xiongshjcode@gmail.com>
2020-03-02 13:45:04 +0800

    feat: add search to city

---
+++ src/views/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style scoped lang="less">
+.search {
+  height: 0.72rem;
+  padding: 0 0.1rem;
+  background: @theme-color;
+  .search-input {
+    width: 100%;
+    height: 0.62rem;
+    padding: 0 0.1rem;
+    box-sizing: border-box;
+    border-radius: 0.06rem;
+    line-height: 0.62rem;
+    text-align: center;
+    color: #666;
+  }
+}
+</style>
