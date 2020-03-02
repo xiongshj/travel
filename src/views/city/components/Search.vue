a03baa6d3f24806a12e95ad6ae992dd03dd93a16
xiongshj <xiongshjcode@gmail.com>
2020-03-02 18:33:55 +0800

    feat: finish search

--- src/views/city/components/Search.vue
+++ src/views/city/components/Search.vue
@@ -1,12 +1,65 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
+    </div>
+    <div v-show="keyword" ref="search" class="search-content">
+      <ul>
+        <li v-for="item of list" class="search-item border-bottom" :key="item.id">{{ item.name }}</li>
+        <li v-show="isListEmpty" class="search-item border-bottom">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data() {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    isListEmpty() {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (const i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (
+              value.spell.indexOf(this.keyword) > -1 ||
+              value.name.indexOf(this.keyword) > -1
+            ) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -24,6 +77,23 @@ export default {
     line-height: 0.62rem;
     text-align: center;
     color: #666;
+  }
+}
+
+.search-content {
+  overflow: hidden;
+  position: absolute;
+  top: 1.58rem;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+  background: #eee;
+  .search-item {
+    padding-left: 0.2rem;
+    line-height: 0.62rem;
+    color: #666;
+    background: @white-color;
   }
 }
 </style>
